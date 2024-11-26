3a50d89ed5db9cfc02b4f818d3492bc75cc24081
ArtEze <passymas@gmail.com>
2024-11-26 16:50:52 -0300

    Update vivaldi_ram.sh

--- vivaldi_ram.sh
+++ vivaldi_ram.sh
@@ -4,24 +4,37 @@
 psave="/initrd/mnt/dev_save"
 n="/0.sesion.vivaldi"
 ram="/tmp/vramfs"
-vtn="/v$RANDOM"
+aleat="$RANDOM"
+vtn="/v$aleat"
+ext=".xz.sfs"
 
 # Compuestos
-sfs="$psave$n.sfs"
-ramsfs="$ram$n.sfs"
+f="$n$ext"
+sfs="$psave$f"
+ramsfs="$ram$f"
 dvt="$ram$vtn" # /tmp/vramfs/v0
 vme="$dvt/merged"
 
 # Programa
-mkdir -pv "$ram"
-mount -t ramfs ramfs "$ram"
-cd "$ram"
-busybox rm -vf "$ramsfs" "$sfs.2.sfs"
-busybox cp -vf "$sfs" "$sfs.2.sfs"
-busybox cp -vf "$sfs.2.sfs" "$ramsfs"
-mkdir -pv "$dvt"
-cd "$dvt"
-montar_sfs_escritura.sh "$ramsfs"
-sleep 1
+if [ -f "$ram/aleat.txt" ];then
+	vtn="/v$(cat $ram/aleat.txt)"
+	dvt="$ram$vtn" # /tmp/vramfs/v0
+	vme="$dvt/merged"
+else
+	echo "$aleat" > "$ram/aleat.txt"
+fi
+echo "vtn '$vtn'"
+if [ ! -d /tmp/vramfs ];then
+	mkdir -pv "$ram"
+	mount -t ramfs ramfs "$ram"
+	cd "$ram"
+	busybox rm -vf "$ramsfs" "$sfs.2.sfs"
+	busybox cp -vf "$sfs" "$sfs.2.sfs"
+	busybox cp -vf "$sfs.2.sfs" "$ramsfs"
+	mkdir -pv "$dvt"
+	cd "$dvt"
+	montar_sfs_escritura "$ramsfs"
+	sleep 1
+fi
 echo "/usr/bin/vivaldi-stable --no-sandbox --user-data-dir='$vme' $@"
 /usr/bin/vivaldi-stable --no-sandbox --user-data-dir="$vme" $@
