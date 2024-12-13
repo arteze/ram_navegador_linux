7be71b317ef96f4e1ccd648829bfb27e3062165a
ArtEze <passymas@gmail.com>
2024-12-13 01:19:43 -0300

    Update pcompress.sh

--- pcompress.sh
+++ pcompress.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 
-# pcompress modificado para comprimir con ia64 y diferenciar .gz.sfs de .xz.sfs
-
 export debug=0 #1 0
 
-nombre_icono="/usr/share/pixmaps/puppy/package_add"
-url_svg="$nombre_icono.svg"
-url_png="$nombre_icono.png"
-if [ -f "$url_svg" ] ; then
-	ZICON="--window-icon=$url_svg"
-fi
-if [ -f "$url_png" ] ; then
-	cambiar_icono.sh "$url_png" 1>/dev/null
+if [ -f /usr/share/pixmaps/puppy/package_add.svg ] ; then
+	ZICON='--window-icon=/usr/share/pixmaps/puppy/package_add.svg'
 fi
 
 #inputfile
 if [ "$1" = "" ]; then #no args, no file
 	export inputfile=$(yad --title="Open File" --file --filename="$pwd" --width="640" --height="480" --center --on-top --sticky --borders="4" $ZICON)
 	[ ! "$inputfile" ] && echo "User cancelled operation" && exit 1
-else
+else 
 	export inputfile="$@" #file was specified in the args
 fi
 [ ! -e "$inputfile" ] && yad --title="Error" --text="File does not exist:\n\n$inputfile" --image="dialog-error" --button="OK":0 --center --on-top --fixed --sticky --borders="4" $ZICON && exit 1
@@ -32,22 +24,13 @@ filenoext="${filenopath/%.$fileext/}"		#file.tar
 fileext2="${filenoext##*.}"					#tar
 __rootname="${filenopath%%.*}"				#file
 
-if [ $debug -eq 1 ] ; then
-	echo "filenopath $filenopath"
-fi
-
 inputdir=${inputfile%/*}
 [[ $inputdir == */ ]] && inputdir=${inputdir%\/}
 export outputdir="$inputdir"
 
-#===============================================================
-#                      MAIN DIALOG
-#===============================================================
-
 CONFIGFILE=${HOME}/.config/pcompress
 [ -f $CONFIGFILE ] && . $CONFIGFILE
 
-#YAD: --field=":CB" "value1!value2!^defaultvalue3!value4!......"
 if [ "$(type 7z 2>/dev/null)" ] ; then
 	formatstr=".7z     0%!.7z     50%!.7z     100%!"
 fi
@@ -57,11 +40,12 @@ fi
 if [ "$(which zip 2>/dev/null)" ] ; then
 	formatstr+=".zip     50%!.zip     100%!"
 fi
-if [ "$(which mksquashfs 2>/dev/null)" ] ; then
-	formatstr+=".gz.sfs gzip!"
-	formatstr+=".xz.sfs xz!"
+if [ "$(which mksquashfs 2>/dev/null)" ] ; then #
+	formatstr+=".sfs     gzip!.gz.sfs gz"
 fi
-
+if [ "$(which mksquashfs 2>/dev/null)" ] ; then 
+	formatstr+=".sfs     xz!.xz.sfs xz"
+fi
 if [ -d "$inputfile" ] ; then
 	labelx="Dir:"
 	formatstr+="!.iso"
@@ -74,10 +58,6 @@ if ! [ "$defaultformat" ] ; then
 	defaultformat='.tar.gz'
 fi
 
-if [ $debug -eq 1 ] ; then
-	echo "formatstr $formatstr"
-fi
-
 formatstr=${formatstr%\!}
 if [ "$outformatcli" ] ; then
 	formatstr=${formatstr//${defaultformat}/^${outformatcli}}
@@ -87,7 +67,7 @@ fi
 
 [ ! "$defaultsplitvalue" ] || [ ! $defaultsplitvalue -ge 1 ] && defaultsplitvalue="100"
 
-dialog_result="$(yad --title="$labelx $inputfile" $ZICON \
+dialog_result=$(yad --title="$labelx $filenopath" $ZICON \
 	--center --fixed --borders="4" --buttons-layout="center" \
 	--columns=2 --separator="|" --form  \
 	--field="Output file:" "$filenopath" \
@@ -101,30 +81,12 @@ dialog_result="$(yad --title="$labelx $inputfile" $ZICON \
 	--field=":NUM" "$defaultsplitvalue" \
 	--field="Generate .sha256.txt file:CHK" "FALSE" \
 	--button="<b>C_reate</b>:0" \
-	--button="gtk-close:10" --dialog-sep --buttons-layout=end )"
+	--button="gtk-close:10" --dialog-sep --buttons-layout=end )
 [ $? -ne 0 ] || [ "$dialog_result" = "" ] && echo "User cancelled operation" >&2 && exit 1
-
-#==============================================================
 
 #                   1           2        3      4     5        6           7       8     9          10
 IFS="|" read -r outfilename outputdir  pass1  pass2   md5   outformat  hidefiles  split  splitvalue  sha256 <<< "$dialog_result"
 #echo read -r $outfilename $outputdir $pass1 $pass2  $md5  $outformat $hidefiles $split $splitvalue $sha256
-
-if [ $debug -eq 1 ] ; then
-	echo "------- Depurando -------"
-	echo "dialog_result $dialog_result"
-	echo "outfilename $outfilename"
-	echo "outputdir $outputdir"
-	echo "pass1 $pass1"
-	echo "pass2 $pass2"
-	echo "md5 $md5"
-	echo "outformat $outformat"
-	echo "hidefiles $hidefiles"
-	echo "split $split"
-	echo "splitvalue $splitvalue"
-	echo "sha256 $sha256"
-	echo "----- Fin Depurando -----"
-fi
 
 SPLITVALUE=
 PASSWORD=
@@ -169,41 +131,11 @@ export OUTPUTFILENAME="${filenopath}${outformat%% *}"
 export OUTPUTFILE="${outputdir}/${OUTPUTFILENAME}"
 export md5 sha256
 
-z="${outformat##* }"
-salida="$outputdir/$outfilename.$z.sfs"
-if [ $debug -eq 1 ] ; then
-	echo "outformat: $outformat"
-	echo "z: $z"
-	echo "Salida: $salida"
-fi
-if [ -f "$salida" ] ; then
-	yad --image="dialog-question" --title "File exists" $ZICON \
-	--button=gtk-yes:0 --button=gtk-no:1 \
-	--text="File: \n\n $salida \n\n already exists. Replace it?   " \
-	--center --sticky --borders=4
-	if [ $? -eq 0 ] ; then
-		rm -fv "$salida"
-	else
-		exit
-	fi
-fi
-
-if [ "$outformat" = ".iso" ] ; then
-	exec dir2iso "$inputfile" "$OUTPUTFILE"
-fi
-
 function pcompressScript() {
+
 	cd "$inputdir" #where the input *file is. [*file=file/dir]
-
 	INPUTFILE="$filenopath"
-	salida="$1"
-	entrada="$inputdir/$INPUTFILE"
 
-	if [ $debug -eq 1 ] ; then
-		echo "inputdir $inputdir"
-		echo "INPUTFILE $INPUTFILE"
-		echo "outformat $outformat"
-	fi
 	case $outformat in
 		.tar*) #SAFE FOR USE WITH LINUX IN GENERAL
 			tarcomp="-vcf"
@@ -214,7 +146,8 @@ function pcompressScript() {
 			esac
 			#tar -vzcf /pathto/filenopath.tar inputfile-no-path
 			PROG="tar $tarcomp"
-		;;
+			;;
+
 		.zip*) #DO NOT USE WITH SYMLINKS
 			[ "$PASSWORD" ] && zippass="-P $PASSWORD"
 			z=${outformat##* }
@@ -224,7 +157,8 @@ function pcompressScript() {
 			esac
 			#zip -rv -5 -P password	/pathto/outfile.zip	infile-no-path
 			PROG="zip -rv $zipcomp $zippass"
-		;;
+			;;
+
 		.7z*) #DO NOT USE WITH SYMLINKS
 			z=${outformat##* }
 			case $z in
@@ -239,36 +173,29 @@ function pcompressScript() {
 			cd "$inputdir"
 			#7z a -t7z -mx9 -pASSWORD -mhe -v100m /path/to/file.7z "infile no path"
 			PROG="7z a -t7z $zcomp $zpass $zhidefiles $zplit"
-		;;
+			;;
+
 		*.sfs*)
-			# mksquashfs input output.sfs -comp xx
 			z=${outformat##* }
+			# gxmessage -center "z es $z"
 			sfscomp="-comp $z"
 			PROG="mksquashfs"
-			OPTS_END=" -noappend $sfscomp -b 1M -Xbcj ia64"
+			OPTS_END="-noappend $sfscomp"
 			INPUTFILE="${OUTPUTFILE}"
 			OUTPUTFILE="$filenopath"
-			if [ $debug -eq 1 ] ; then
-				echo "sfscomp $sfscomp"
-				echo "z $z"
-				echo "linea $LINENO ... INPUTFILE $INPUTFILE"
-				echo "linea $LINENO ... OUTPUTFILE $OUTPUTFILE"
-				echo "linea $LINENO ... outfilename $outfilename"
-			fi
-		;;
+			;;
 	esac
-	if [ $debug -eq 1 ] ; then
-		echo "OUTPUTFILE: $OUTPUTFILE"
-	fi
-	echo ""
+
+	echo "File: $OUTPUTFILE"
+	echo
 	if ! [ "$PASSWORD" ] || [ $debug -eq 1 ] ; then
-		echo $PROG $OPTS "$entrada" "$salida" $OPTS_END
+		echo $PROG $OPTS "$OUTPUTFILE" "$INPUTFILE" $OPTS_END
 		echo
 	fi
-	$PROG $OPTS "$entrada" "$salida" $OPTS_END
+	$PROG $OPTS "$OUTPUTFILE" "$INPUTFILE" $OPTS_END
 	exitcode=$?
 	if [ $exitcode -ne 0 ]; then
-		echo
+		echo 
 		echo "Exit code: $exitcode"
 		echo -n 'THERE WAS AN ERROR. PRESS ENTER KEY TO CLOSE THIS WINDOW: '
 		read var #read -t 30 var (timeout)
@@ -291,7 +218,21 @@ function pcompressScript() {
 }
 export -f pcompressScript
 
-xdotool getactivewindow set_window --name "Creando '$salida'"
-pcompressScript "$salida"
+if [ -f "$OUTPUTFILE" ] ; then
+	yad --image="dialog-question" --title "File exists" $ZICON \
+	--button=gtk-yes:0 --button=gtk-no:1 \
+	--text="File: \n\n $OUTPUTFILE \n\n already exists. Replace it?   " \
+	--center --sticky --borders=4
+	if [ $? -eq 0 ] ; then
+		rm -f "$OUTPUTFILE"
+	else
+		exit
+	fi
+fi
 
-### END ###
+if [ "$outformat" = ".iso" ] ; then
+	exec dir2iso "$inputfile" "$OUTPUTFILE"
+fi
+
+cambiar_titulo.sh "Comprimiendo"
+pcompressScript
